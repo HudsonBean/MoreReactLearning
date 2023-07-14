4f04477ff37b2355f8e886b6e9d1b91ff90946a7
Hudson <hudsonbeandev@gmail.com>
2023-07-14 11:41:49 -0500

    Finished making the form

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Styles/App.css";
 
 function App() {
+  //Variables
   const [person, setPerson] = useState({
     data: {
       firstName: "John",
@@ -16,9 +17,29 @@ function App() {
         "https://upload.wikimedia.org/wikipedia/commons/7/7e/Roblox_Logo_2022.jpg?20220830054427",
     },
   });
+  let newPerson = {
+    ...person,
+    data: {
+      ...person.data,
+    },
+    creation: {
+      ...person.creation,
+    },
+  };
+  //Functions
+  function Input(props) {
+    return (
+      <input
+        type={"text"}
+        name={props.property}
+        defaultValue={person[props.parent][props.property]}
+        onChange={props.onEdit}
+      ></input>
+    );
+  }
   function handleSubmit(e) {
     e.preventDefault();
-    console.log("test");
+    setPerson(newPerson);
   }
   return (
     <div className="container">
@@ -28,51 +49,112 @@ function App() {
             <h1>Person:</h1>
             {/* First Name */}
             <label htmlFor={"firstName"}>First Name:</label>
-            <input
-              type={"text"}
-              name={"firstName"}
-              value={person.data.firstName}
-            ></input>
+            <Input
+              onEdit={(e) => {
+                newPerson = {
+                  ...newPerson,
+                  data: {
+                    ...newPerson.data,
+                    firstName: e.target.value,
+                  },
+                };
+              }}
+              parent={"data"}
+              property={"firstName"}
+            />
             {/* Last Name */}
             <label htmlFor={"lastName"}>Last Name:</label>
-            <input
-              type={"text"}
-              name={"lastName"}
-              value={person.data.lastName}
-            ></input>
+            <Input
+              onEdit={(e) => {
+                newPerson = {
+                  ...newPerson,
+                  data: {
+                    ...newPerson.data,
+                    lastName: e.target.value,
+                  },
+                };
+              }}
+              parent={"data"}
+              property={"lastName"}
+            />
             {/* Age */}
             <label htmlFor={"age"}>Age:</label>
-            <input type={"text"} name={"age"} value={person.data.age}></input>
+            <Input
+              onEdit={(e) => {
+                newPerson = {
+                  ...newPerson,
+                  data: {
+                    ...newPerson.data,
+                    age: e.target.value,
+                  },
+                };
+              }}
+              parent={"data"}
+              property={"age"}
+            />
             {/* Gender */}
             <label htmlFor={"gender"}>Gender:</label>
-            <input
-              type={"text"}
-              name={"gender"}
-              value={person.data.gender}
-            ></input>
+            <Input
+              onEdit={(e) => {
+                newPerson = {
+                  ...newPerson,
+                  data: {
+                    ...newPerson.data,
+                    gender: e.target.value,
+                  },
+                };
+              }}
+              parent={"data"}
+              property={"gender"}
+            />
           </div>
           <div className="creation-container">
             <h1>Creation:</h1>
             {/* Name */}
             <label htmlFor={"name"}>Name:</label>
-            <input
-              type={"text"}
-              name={"name"}
-              value={person.creation.name}
-            ></input>
+            <Input
+              onEdit={(e) => {
+                newPerson = {
+                  ...newPerson,
+                  creation: {
+                    ...newPerson.creation,
+                    name: e.target.value,
+                  },
+                };
+              }}
+              parent={"creation"}
+              property={"name"}
+            />
             {/* Date Created */}
             <label htmlFor={"dateCreated"}>Date Created:</label>
-            <input
-              name={"dateCreated"}
-              value={person.creation.dateCreated}
-            ></input>
+            <Input
+              onEdit={(e) => {
+                newPerson = {
+                  ...newPerson,
+                  creation: {
+                    ...newPerson.creation,
+                    dateCreated: e.target.value,
+                  },
+                };
+              }}
+              parent={"creation"}
+              property={"dateCreated"}
+            />
             {/* Image */}
             <label htmlFor={"image"}>Image:</label>
-            <input
-              type={"text"}
-              name={"image"}
-              value={person.creation.image}
-            ></input>
+            <Input
+              onEdit={(e) => {
+                newPerson = {
+                  ...newPerson,
+                  creation: {
+                    ...newPerson.creation,
+                    image: e.target.value,
+                  },
+                };
+              }}
+              parent={"creation"}
+              property={"image"}
+            />
           </div>
         </div>
         <button type="submit">Submit</button>
@@ -104,7 +186,7 @@ function App() {
           <span className="data">
             <img
               src={person.creation.image}
-              alt={"A picture of the person's creation."}
+              alt={"The person's creation."}
               width={"50px"}
               height={"50px"}
             ></img>
