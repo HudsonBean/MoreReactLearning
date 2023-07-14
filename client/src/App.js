7e600311879267e678be31fefe6ef625ea73b02e
Hudson <hudsonbeandev@gmail.com>
2023-07-14 00:52:23 -0500

    Making useState with array and using the data

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./Styles/App.css";
 
 function App() {
+  const [person, setPerson] = useState({
+    data: {
+      firstName: "John",
+      lastName: "Doe",
+      age: 69,
+      gender: "Male",
+    },
+    creation: {
+      name: "ROBLOX",
+      dateCreated: "2007",
+      image: "https://imgur.com/iOtA0vX",
+    },
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Person:</h1>
+      {/* First Name */}
+      <label htmlFor={"firstName"}>First Name:</label>
+      <input name={"firstName"} value={person.data.firstName}></input>
+      {/* Last Name */}
+      <label htmlFor={"firstName"}>First Name:</label>
+      <input name={"firstName"} value={person.data.firstName}></input>
+      {/* Age */}
+      <label htmlFor={"firstName"}>First Name:</label>
+      <input name={"firstName"} value={person.data.firstName}></input>
+      {/* Gender */}
+      <label htmlFor={"firstName"}>First Name:</label>
+      <input name={"firstName"} value={person.data.firstName}></input>
     </div>
   );
 }
